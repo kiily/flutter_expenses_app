ed8b724c0b0477cf8209c802fa6d0df7eda254a0
kiily <miguelmarin95@hotmail.com>
2020-06-03 00:40:30 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import './transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'new shoes2',
+      amount: 89.99,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(children: <Widget>[
+                Container(
+                  child: Text(tx.amount.toString()),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(tx.title),
+                      Text(tx.date.toString())
+                    ],
+                  ),
+                )
+              ]),
+            );
+          }).toList()),
+        ],
+      ),
+    );
+  }
+}
